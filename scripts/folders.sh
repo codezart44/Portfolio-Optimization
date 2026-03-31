1e19279b2c0d767782841dce5382b27149fa6e83
oskarew <oswa.public@gmail.com>
2026-03-31 13:54:38 -0700

    data folder setup

---
+++ scripts/folders.sh
@@ -0,0 +1,1 @@
+mkdir -p data/{alpha,macro,ohlcv,return,riskmodel}
--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
-echo "exporting fred api key"
+echo "> exporting fred api key"
 source scripts/env.sh
 
-echo "downloading return data"
+echo "> setting up data folders"
+source scripts/folders.sh
+
+echo "> downloading return data"
 python scripts/download.py
 
-echo "processing risk models"
+echo "> processing risk models"
 python scripts/risk.py
